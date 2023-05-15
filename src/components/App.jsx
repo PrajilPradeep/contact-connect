9e5f9c60adaad4277fe695cf588dcf0574156b6c
Prajil <Prajil7425@gmail.com>
2023-05-15 15:57:47 +0530

    Add ContactList component to display contacts

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,11 +2,13 @@ import { useState } from "react";
 import "../styles/App.css";
 import Header from "./Header.jsx";
 import AddContact from "./AddContact.jsx";
+import ContactList from "./ContactList.jsx";
 function App() {
   return (
     <>
       <Header />
       <AddContact />
+      <ContactList />
     </>
   );
 }
---
+++ src/components/ContactList.jsx
@@ -0,0 +1,5 @@
+function ContactList() {
+  return <div className="ui celled list">Contact list</div>;
+}
+
+export default ContactList;
