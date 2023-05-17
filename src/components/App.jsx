98b6f5d6198ed207c4c9e26742bccf96a3b97e15
Prajil <Prajil7425@gmail.com>
2023-05-17 08:36:50 +0530

    Added unique ids for the contact list

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,30 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "../styles/App.css";
 import Header from "./Header.jsx";
 import AddContact from "./AddContact.jsx";
 import ContactList from "./ContactList.jsx";
+import { v4 as uuidv4 } from "uuid";
+
 function App() {
   const [contactList, setContactList] = useState([]);
+  console.log(uuidv4());
   const addContactHandler = (contact) => {
-    const updatedContactList = [...contactList, contact];
+    const updatedContactList = [...contactList, { id: uuidv4(), ...contact }];
+    window.localStorage.setItem(
+      "contactList",
+      JSON.stringify(updatedContactList)
+    );
     setContactList(updatedContactList);
   };
+
+  useEffect(() => {
+    const contactsFromLocalStorage = JSON.parse(
+      localStorage.getItem("contactList")
+    );
+    if (contactsFromLocalStorage) {
+      setContactList(contactsFromLocalStorage);
+    }
+  }, []);
   return (
     <>
       <Header />
